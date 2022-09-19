e885b91cde24c5d34d208f93b86447b897086db1
Git like me <aaronpqking.gmail.com>
2022-09-18 21:18:38 -0400

    cleaned up JS

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -3,37 +3,27 @@
 function renderLicenseBadge(license) {
   if (license === "Apache") {
     return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-  }
-  else if (license === "MIT") {
+  } else if (license === "MIT") {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-  }
-  else if (license === "ISC") {
+  } else if (license === "ISC") {
     return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
-  }
-  else if (license === "Mozilla") {
+  } else if (license === "Mozilla") {
     return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
-  }
-  else (license === " ");
-  }
-
+  } else license === " ";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === "Apache") {
     return `https://opensource.org/licenses/Apache-2.0`;
-  }
- else  if (license === "MIT") {
-    return `https://opensource.org/licenses/MIT;`
-  }
-  else if (license === "ISC") {
-    return 'https://opensource.org/licenses/ISC'
-  }
-  else if (license === "Mozilla") {
-    return 'https://opensource.org/licenses/MPL-2.0'
-
-  }
-  else (license === " ")
+  } else if (license === "MIT") {
+    return `https://opensource.org/licenses/MIT;`;
+  } else if (license === "ISC") {
+    return "https://opensource.org/licenses/ISC";
+  } else if (license === "Mozilla") {
+    return "https://opensource.org/licenses/MPL-2.0";
+  } else license === " ";
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -68,7 +58,6 @@ ${data.credit}
 ## Contact Info
 ${data.contact}
 `;
-  
 }
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,107 +1,120 @@
 // TODO: Include packages needed for this application
 const generateMarkdown = require("./generateMarkdown");
 const fs = require("fs");
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const { title } = require("process");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        message: 'What is the project title?',
-        name: 'title',
-        default: title,
-        validate: (value) => { if (value) { return true } else{return 'enter a title to continue' }},
+  {
+    type: "input",
+    message: "What is the project title?",
+    name: "title",
+    default: title,
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "enter a title to continue";
+      }
     },
-     {
-        type: 'input',
-        message: 'how do you install your app?',
-        name: 'installation',
-        validate: (value) => { if (value) { return true } else{return 'enter a installation details to continue' }},
-     },
-      {
-        type: 'input',
-        message: 'add instructions here',
-        name: 'instructions',
-        validate: (value) => { if (value) { return true } else{return 'enter app instructions to continue' }},
+  },
+  {
+    type: "input",
+    message: "how do you install your app?",
+    name: "installation",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "enter a installation details to continue";
+      }
     },
-     {
-        type: 'input',
-        message: 'What\'s your app all about?',
-        name: 'description',
-        validate: (value) => { if (value) { return true } else{return 'enter a title to continue' }},
+  },
+  {
+    type: "input",
+    message: "add instructions here",
+    name: "instructions",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "enter app instructions to continue";
+      }
     },
-     {
-        type: 'input',
-        message: 'How do you use your app?',
-        name: 'usage',
-        validate: (value) => { if (value) { return true } else{return 'enter a title to continue' }},
+  },
+  {
+    type: "input",
+    message: "What's your app all about?",
+    name: "description",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "enter a title to continue";
+      }
     },
-     {
-        type: 'list',
-         message: 'What licenses did you use?',
-        choices:["Apache", "MIT", "ISC", "Mozilla", "None"],
-        name: 'license',
-        validate: (value) => { if (value) { return true } else{return 'enter a title to continue' }},
-     },
-       {
-        type: 'input',
-           message: 'Who contributed to the project?',
-        name: 'Credits',
-        validate: (value) => { if (value) { return true } else{return 'enter a title to continue' }},
+  },
+  {
+    type: "input",
+    message: "How do you use your app?",
+    name: "usage",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "enter a title to continue";
+      }
     },
-     {
-        type: 'input',
-        message: 'contact info',
-        name: 'Contact',
-        validate: (value) => { if (value) { return true } else{return 'enter a title to continue' }},
-    }
+  },
+  {
+    type: "list",
+    message: "What licenses did you use?",
+    choices: ["Apache", "MIT", "ISC", "Mozilla", "None"],
+    name: "license",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "enter a title to continue";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "Who contributed to the project?",
+    name: "Credits",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "enter a title to continue";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "contact info",
+    name: "Contact",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "enter a title to continue";
+      }
+    },
+  },
 ];
 
 // TODO: Create a function to write README file
-writeToFile = (fileName, data)=> {
-
-}
+writeToFile = (fileName, data) => {};
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
-        .then(data => {
-        fs.writeFileSync("README.md", generateMarkdown(data))
-    })
+  inquirer.prompt(questions).then((data) => {
+    fs.writeFileSync("README.md", generateMarkdown(data));
+  });
 }
 
 // Function call to initialize app
 init();
-
-// Init2();
-// function Init2() {
-//     inquirer.prompt([
-//         {
-//             type: "list",
-//             name: "answers",
-//             message: "What do you want?!",
-//             choices:["nothing", "everything", "something", "anything"," "]
-//         }
-//     ])
-//         .then(data => {
-//         switch (data.answers) {
-//             case "nothing": 
-//                 console.log("Nothing!")
-//                 break;
-//             case "everything":
-//                 console.log("everything")
-//                 break;
-//             case "something":
-//                 console.log("something")
-//                 break;
-//             case "anything":
-//                 console.log("anything")
-//             default:"fuck off"
-//                 console.log("fuck off!")
-//                 break;
-//         }
-//     })
-// }
-
- 
